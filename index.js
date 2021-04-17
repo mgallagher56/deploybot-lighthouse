5e2d4d024cefcdd4f130ec8dbb9b02a0615f1aaf
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-04-17 13:19:44 +0100

    updated mongo url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./models/lighthouse');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+mongoose.connect(process.env.MONGODB_URL || `mongodb://localhost:27017/node-react-starter`);
 
 app.use(bodyParser.json());
 
